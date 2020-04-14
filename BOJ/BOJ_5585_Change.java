e216f854794a0934d31bb0f8cffc3b4e44e4327d
Kimhayoung <khy07181@gmail.com>
2020-04-14 15:16:37 +0900

    거스름돈(5585) upload

---
+++ BOJ/BOJ_5585_Change.java
@@ -0,0 +1,24 @@
+package Step16;
+
+import java.util.Scanner;
+
+public class BOJ_5585_Change {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int money = 1000 - scanner.nextInt();
+		int currency[] = { 500, 100, 50, 10, 5, 1 };
+		int count = 0;
+		
+		for (int i = 0; i < currency.length; i++) {
+			count += money / currency[i];
+			money %= currency[i];
+			
+			if(money == 0) {
+				System.out.println(count);
+				break;
+			}
+		}
+	}
+
+}
