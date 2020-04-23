54c01a4bb50421c9d629a4ddcb9ecc97d17c128e
Kimhayoung <khy07181@gmail.com>
2020-04-23 16:08:59 +0900

    다음 큰 숫자 upload

---
+++ Programmers/level2/Solution.java
@@ -0,0 +1,24 @@
+package level2;
+
+class Solution {
+	public int solution(int n) {
+		int answer = 0;
+		for(answer = n + 1; ; answer++) {
+			if(count1(n) == count1(answer)) {
+				break;
+			}
+		}
+		return answer;
+	}
+
+	public int count1(int n) {
+		int cnt = 0;
+		while (n != 1) {
+			if(n%2 != 0) {
+				cnt++;
+			}
+			n /= 2;
+		}
+		return cnt;
+	}
+}
