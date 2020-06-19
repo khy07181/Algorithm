fcc2fae8528ebbebb538f9480b5340a2c3a6947e
Kimhayoung <khy07181@gmail.com>
2020-06-19 13:43:36 +0900

    서울에서 김서방 찾기 upload

---
+++ Programmers/level1/서울에서_김서방_찾기.java
@@ -0,0 +1,20 @@
+package level1;
+
+public class 서울에서_김서방_찾기 {
+	public static String solution(String[] seoul) {
+        String answer = "";
+        for(int i = 0; i < seoul.length; i++) {
+        	if(seoul[i] == "Kim") {
+        		answer = "김서방은 " + i + "에 있다";
+        		break;
+        	}
+        }
+        return answer;
+    }
+
+	public static void main(String[] args) {
+		String[] seoul = {"Jane", "Kim"};
+		System.out.println(solution(seoul));
+	}
+
+}
