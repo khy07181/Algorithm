937cdf117cc169b9e1467a3044285a8dacbc1874
kimhayoung <qlalzl9@naver.com>
2020-08-30 22:20:18 +0900

    전자레인지(10162) upload

---
+++ BOJ/BOJ_10162_Microwave.java
@@ -0,0 +1,29 @@
+package Greedy_Algorithm;
+
+import java.util.Scanner;
+
+public class BOJ_10162_Microwave {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int T = sc.nextInt();
+		int A = 0;
+		int B = 0;
+		int C = 0;
+
+		// 300 60 10
+		if (T % 10 != 0) {
+			System.out.println(-1);
+		} else {
+			A = T / 300;
+			T %= 300;
+
+			B = T / 60;
+			T %= 60;
+
+			C = T / 10;
+			T %= 10;
+			System.out.println(A + " " + B + " " + C);
+		}
+	}
+}
