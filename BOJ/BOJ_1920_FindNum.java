0f4894e9dc0a743d16805a0360a6e309c94a1e46
kimhayoung <qlalzl9@naver.com>
2020-09-05 01:50:27 +0900

    수 찾기(1920) upload

---
+++ BOJ/BOJ_1920_FindNum.java
@@ -0,0 +1,33 @@
+package Greedy_Algorithm;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class BOJ_1920_FindNum {
+
+	static int N, M, answer;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+
+		HashSet<Integer> A = new HashSet<>();
+
+		for (int i = 0; i < N; i++) {
+			A.add(sc.nextInt());
+		}
+
+		M = sc.nextInt();
+
+		for (int i = 0; i < M; i++) {
+			int arrM = sc.nextInt();
+			if (A.contains(arrM)) {
+				answer = 1;
+			} else {
+				answer = 0;
+			}
+			System.out.println(answer);
+		}
+	}
+}
+ 
