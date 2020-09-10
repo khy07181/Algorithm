62209c8816bdbba797ad78d7bdf7ab8f458c163f
kimhayoung <qlalzl9@naver.com>
2020-09-10 22:31:49 +0900

    완주하지 못한 선수 upload

---
+++ Programmers/level1/완주하지_못한_선수.java
@@ -0,0 +1,17 @@
+package level1;
+
+import java.util.Arrays;
+
+public class 완주하지_못한_선수 {
+	public String solution(String[] participant, String[] completion) {
+		Arrays.sort(participant);
+		Arrays.sort(completion);
+		
+		for (int i = 0; i < completion.length; i++) {
+			if (!participant[i].equals(completion[i])) {
+				return participant[i];
+			}
+		}
+		return participant[participant.length -1];
+	}
+}
