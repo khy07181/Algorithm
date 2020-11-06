604945b5709b05477c7412d3c8e033d09b44e62e
kimhayoung <qlalzl9@naver.com>
2020-11-06 15:15:44 +0900

    두개 뽑아서 더하기 upload

--- Programmers/level1/두_개_뽑아서_더하기.java
+++ Programmers/level1/두_개_뽑아서_더하기.java
@@ -1,5 +1,28 @@
 package level1;
 
-public class 두_개_뽑아서_더하기 {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 
+public class 두_개_뽑아서_더하기 {
+	public int[] solution(int[] numbers) {
+        HashSet<Integer> set = new HashSet();
+        
+        for(int i = 0; i < numbers.length - 1; i++) {
+        	for(int j = i + 1; j < numbers.length; j++) {
+        		set.add(numbers[i] + numbers[j]);
+        	}
+        }
+        int[] answer = new int[set.size()];
+        ArrayList<Integer> list = new ArrayList<>(set);
+        Collections.sort(list);
+        
+        for(int i = 0; i < list.size(); i++) {
+        	answer[i] = list.get(i);
+        }
+        return answer;
+    }
+	public static void main(String[] args) {
+		
+	}
 }
