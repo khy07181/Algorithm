e619f1e0544ac5c52295434b23c091ec22d8849a
kimhayoung <qlalzl9@naver.com>
2021-01-02 16:26:09 +0900

    2562 : 최댓값

---
+++ BOJ/BOJ_2562.java
@@ -0,0 +1,22 @@
+package baekjoon;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class BOJ_2562 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        LinkedList<Integer> list = new LinkedList<>();
+        int max = 0;
+
+        for (int i = 0; i < 9; i++) {
+            int num = sc.nextInt();
+            if(num > max) {
+                max = num;
+            }
+            list.add(num);
+        }
+        System.out.println(max);
+        System.out.println(list.indexOf(max) + 1);
+    }
+}
