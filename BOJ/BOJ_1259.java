0905e462deb0673177ae124b92490ab130372e29
kimhayoung <qlalzl9@naver.com>
2021-01-06 17:22:33 +0900

    .gitignore 추가

---
+++ BOJ/BOJ_1259.java
@@ -0,0 +1,26 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class BOJ_1259 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String strNum = "";
+
+        while (true) {
+            strNum = sc.nextLine();
+            if (strNum.equals("0")) {
+                break;
+            }
+            int num = Integer.valueOf(strNum);
+            StringBuffer sb = new StringBuffer();
+            sb.append(strNum);
+            int num2 = Integer.valueOf(String.valueOf(sb.reverse()));
+            if (num == num2) {
+                System.out.println("yes");
+            } else {
+                System.out.println("no");
+            }
+        }
+    }
+}
