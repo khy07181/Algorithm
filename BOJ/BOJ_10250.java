bfc2edae7e626e09c9a9fc3ecb13c81081bdc528
kimhayoung <qlalzl9@naver.com>
2021-01-07 17:28:01 +0900

    10250 : ACM 호텔

--- BOJ/BOJ_10250.java
+++ BOJ/BOJ_10250.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class BOJ_10250 {
+import java.util.Scanner;
+
+public class BOJ_10250 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int T = scanner.nextInt();
+
+        for (int i = 0; i < T; i++) {
+            int H = scanner.nextInt();
+            int W = scanner.nextInt();
+            int N = scanner.nextInt();
+            int h, w;
+
+            if (N % H != 0) {
+                h = N % H;
+                w = N / H + 1;
+            } else {
+                h = H;
+                w = N / H;
+            }
+            System.out.println(100 * h + w);
+        }
+
+    }
 }
