0aa9578269611c69c4b5a04bf83b4762a660bf7c
kimhayoung <qlalzl9@naver.com>
2021-01-08 17:29:33 +0900

    11720 : 숫자의 합

--- BOJ/BOJ_11720.java
+++ BOJ/BOJ_11720.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class BOJ_11720 {
+import java.util.Scanner;
+
+public class BOJ_11720 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        String str = sc.next();
+
+        int result = 0;
+        for (int i = 0; i < N; i++) {
+            result += Integer.valueOf(str.split("")[i]);
+        }
+
+        System.out.println(result);
+    }
 }
