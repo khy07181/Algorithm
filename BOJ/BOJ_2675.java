b9a4fff1f50626c5aa3a041a02fc477d28e17b0a
kimhayoung <qlalzl9@naver.com>
2021-01-11 17:31:12 +0900

    2675 : 문자열 반복

--- BOJ/BOJ_2675.java
+++ BOJ/BOJ_2675.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class BOJ_2675 {
+import java.util.Scanner;
+
+public class BOJ_2675 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        for (int i = 0; i < T; i++) {
+            int R = sc.nextInt();
+            String S = sc.next();
+            StringBuffer sb = new StringBuffer();
+
+            for (int j = 0; j < S.length(); j++) {
+                for (int k = 0; k < R; k++) {
+                    sb.append(S.charAt(j));
+                }
+            }
+            System.out.println(sb.toString());
+        }
+    }
 }
