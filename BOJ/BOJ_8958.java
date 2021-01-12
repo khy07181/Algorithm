f73c067312e47f018dcb9b8a3bc260bae3170e7e
kimhayoung <qlalzl9@naver.com>
2021-01-12 17:32:07 +0900

    8958 : OX퀴즈

--- BOJ/BOJ_8958.java
+++ BOJ/BOJ_8958.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class BOJ_8958 {
+import java.util.Scanner;
+
+public class BOJ_8958 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        for (int i = 0; i < T; i++) {
+            String s = sc.next();
+            StringBuffer sb = new StringBuffer();
+            sb.append(s);
+
+            int score = 0;
+            int total = 0;
+
+            for (int j = 0; j < sb.length(); j++) {
+                if (sb.charAt(j) == 'O') {
+                    score++;
+                    total += score;
+                } else {
+                    score = 0;
+                }
+            }
+            System.out.println(total);
+        }
+    }
 }
