73b17e188a10ae2a36a4ecf9fb12a31d92fcda33
kimhayoung <qlalzl9@naver.com>
2021-01-21 16:22:18 +0900

    최솟값 만들기

--- Programmers/level2/최솟값_만들기.java
+++ Programmers/level2/최솟값_만들기.java
@@ -1,2 +1,21 @@
-package level2;public class 최솟값_만들기 {
+package level2;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class 최솟값_만들기 {
+    public int solution(int[] A, int[] B) {
+        int answer = 0;
+        Integer[] B2 = new Integer[B.length];
+        for (int i = 0; i < B.length; i++) {
+            B2[i] = B[i];
+        }
+        Arrays.sort(A);
+        Arrays.sort(B2, Collections.reverseOrder());
+
+        for(int i = 0; i < A.length; i++) {
+            answer += A[i] * B2[i];
+        }
+        return answer;
+    }
 }
