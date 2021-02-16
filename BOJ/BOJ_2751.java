2ae803b9c85a94fb1ac806d9aa14e94647f7403f
kimhayoung <qlalzl9@naver.com>
2021-02-16 21:50:14 +0900

    2751 : 수 정렬하기2

--- BOJ/BOJ_2751.java
+++ BOJ/BOJ_2751.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class BOJ_2751 {
+import java.io.*;
+import java.util.Collections;
+import java.util.LinkedList;
+
+public class BOJ_2751 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        LinkedList<Integer> list = new LinkedList<>();
+
+        for (int i = 0; i < N; i++) {
+            list.add(Integer.parseInt(br.readLine()));
+        }
+
+        Collections.sort(list);
+        StringBuffer sb = new StringBuffer();
+
+        for (int value : list) {
+            sb.append(value).append("\n");
+        }
+        System.out.println(sb.toString());
+    }
 }
