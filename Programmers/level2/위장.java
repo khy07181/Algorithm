eca73a2365fa1608eb72687d9b4f2edc549717c1
kimhayoung <qlalzl9@naver.com>
2021-04-06 00:10:28 +0900

    add: 위장

---
+++ Programmers/level2/위장.java
@@ -0,0 +1,25 @@
+package level2;
+
+import java.util.HashMap;
+
+public class 위장 {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < clothes.length; i++) {
+            if (map.get(clothes[i][1]) == null) {
+                map.put(clothes[i][1], 1);
+            } else {
+                map.put(clothes[i][1], map.get(clothes[i][1]) + 1);
+            }
+        }
+
+        for (String key : map.keySet()) {
+            answer *= (map.get(key) + 1);
+        }
+        answer--;
+
+        return answer;
+    }
+}
