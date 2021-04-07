c8709e82679b6ed2898f16c1ff118db174503c67
kimhayoung <qlalzl9@naver.com>
2021-04-07 22:34:55 +0900

    add: 가장 큰 수

--- Programmers/level2/가장_큰_수.java
+++ Programmers/level2/가장_큰_수.java
@@ -4,24 +4,22 @@ import java.util.Arrays;
 
 public class 가장_큰_수 {
 	public String solution(int[] numbers) {
-		String[] strArr = new String[numbers.length];
-
-		for (int i = 0; i < strArr.length; i++) {
-			strArr[i] = Integer.toString(numbers[i]);
+		String[] strNumbers = new String[numbers.length];
+		for (int i = 0; i < numbers.length; i++) {
+			strNumbers[i] = String.valueOf(numbers[i]);
 		}
 
-		Arrays.sort(strArr, String.CASE_INSENSITIVE_ORDER);
+		Arrays.sort(strNumbers, (s1, s2) -> (s2 + s1).compareTo(s1 + s2));
+
+		if(strNumbers[0].equals("0")) {
+			return "0";
+		}
+
 		String answer = "";
-
-		for (int i = strArr.length - 1; i >= 0; i--) {
-			answer += strArr[i];
+		for (String a : strNumbers) {
+			answer += a;
 		}
-		return answer;
-	}
 
-	public static void main(String[] args) {
-		int [] numbers = {6, 10 ,2};
-		가장_큰_수 test = new 가장_큰_수();
-		System.out.println(test.solution(numbers));
+		return answer;
 	}
 }
